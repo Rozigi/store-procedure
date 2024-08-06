1964d3f494513a356067f389a0487d38692f4d1b
Rozigi <reozigi2005@gmail.com>
2024-08-06 18:59:56 -0500

    Add files via upload

---
+++ SQLQuery9StoreProcedure_Tech48.sql
@@ -0,0 +1,15 @@
+/**
+Assignment For The Week
+1- Create Store Procedures using these tables
+-World, Games, CompanyInfo, EmployeeInfo and Business Table
+
+2-Upload The Store Procedures In GitHub
+
+3- Review The ERD Notes https://www.lucidchart.com/pages/er-diagrams
+
+4- Create ERD Diagram In SQL Server with the Car Dealer assignment
+
+5-Learn About Sharepoint Site and Confluence (Watch Youtube Video or Google It)
+
+**/
+
